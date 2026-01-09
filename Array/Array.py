4b3c21306a2442ef5922f5b493f81b94f2841d77
OmJoshi02 <omj020705@gmail.com>
2026-01-09 12:42:16 +0530

    Array Demonstration

---
+++ Array/Array.py
@@ -0,0 +1,65 @@
+#import array module
+
+from array import *
+
+#integer array
+integer = array('i', [1,2,3,4])
+# print(integer)
+
+#character array
+char = array('u', ['a','b','c','d','e','f'])
+# print(char)
+
+#Size of array
+print("size of array : ",len(integer))
+
+print()
+
+#Normal for loop
+print("Integer array")
+print("typecode : ", integer.typecode)
+for i in range(0, len(integer)):
+    print(integer[i], end= ", ")
+    
+print("\n")
+
+print("Character array")
+print("typecode : ", char.typecode)
+for i in range(0, len(char)):
+    print(char[i], end= ", ")
+    
+print("\n")
+
+#Enhanced for loop
+print("Enhanced for loops")
+for x in integer:
+    print(x, end= ", ")
+
+print()
+
+for x in char:
+    print(x, end=", ")
+    
+
+print("\n")
+#Reverse the array
+
+integer.reverse()
+
+for i in integer:
+    print(i, end= ", ")
+    
+
+print("\n")
+
+
+#Insert element
+integer.insert(1,50)
+for i in integer:
+    print(i, end= ", ")
+    
+print("\n")
+#Insert element at last
+integer.append(100)
+for i in integer:
+    print(i, end= ", ")
